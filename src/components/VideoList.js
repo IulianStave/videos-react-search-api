61fe2e0fd7545d495d4c3835745c96b0b64cb692
iulianstave <iulian.staverescu@gmail.com>
2019-12-18 18:26:48 +0200

    Add video thumbnails

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -4,7 +4,7 @@ import VideoItem from './VideoItem';
 const VideoList = ({videos, source }) => {
   // props.videos
   const renderedList = videos.map((video) => {
-    return <VideoItem/>;
+    return <VideoItem video={video}/>;
   });
   return (
     <div className="ui segment">
