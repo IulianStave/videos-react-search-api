68cb072248d9a7d32cd4e46f8bb53e6192dcd6f0
iulianstave <iulian.staverescu@gmail.com>
2019-12-18 13:09:29 +0200

    Add onChange event handler callback method

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,10 +1,16 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+state = {term: ''};
 
-onFormSubmit = event => {
+onInputChange = (event) => {
+  this.setState({term: event.target.value});
+};
+
+onFormSubmit = (event) => {
   event.preventDefault();
-}
+  
+};
   
   render() {
     return (
@@ -12,7 +18,14 @@ onFormSubmit = event => {
         <form onSubmit={this.onFormSubmit} className="ui form">
           <div className="field">
             <label htmlFor="searchInput">Search for videos:</label>
-            <input type="text" className="ui input" id="searchInput"/>
+            <input 
+              type="text" 
+              className="ui input" 
+              id="searchInput"
+              value={this.state.term}
+              // onChange={e => this.setState({term: e.target.value})}
+              onChange={this.onInputChange}
+            />
           </div>
         </form>
       </div>
